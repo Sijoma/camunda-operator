04003f655b1df8d1df1e097af124dfcd5204a62b
Simon Zengerling <simon.zengerling@camunda.com>
2025-07-08 23:18:58 +0200

    feat: common labels

--- pkg/labels/labels.go
+++ pkg/labels/labels.go
@@ -3,25 +3,26 @@ package labels
 import corev1alpha1 "github.com/camunda/camunda-operator/api/v1alpha1"
 
 func Create(osc *corev1alpha1.OrchestrationCluster) map[string]string {
-	return map[string]string{
-		"app":                          "camunda-platform",
-		"app.kubernetes.io/name":       "camunda-platform",
-		"app.kubernetes.io/instance":   osc.Name,
-		"app.kubernetes.io/component":  "core",
-		"app.kubernetes.io/part-of":    "camunda-platform",
-		"app.kubernetes.io/managed-by": "orchestrationcluster-controller",
-		"app.kubernetes.io/version":    osc.Spec.Version,
-	}
+	l := commonLabels(osc)
+	l["app.kubernetes.io/version"] = osc.Spec.Version
+	return l
 }
 
-// CreateSelector creates the selector labels for resources
+// CreateSelector creates the selector labels for resources.
+// This should be used for labelSelector fields that are immutable, such as StatefulSets.
 func CreateSelector(osc *corev1alpha1.OrchestrationCluster) map[string]string {
+	return commonLabels(osc)
+}
+
+// commonLabels returns common labels for the OrchestrationCluster.
+// The version is not included as it changes frequently and should not be used for a label selector for example.
+func commonLabels(osc *corev1alpha1.OrchestrationCluster) map[string]string {
 	return map[string]string{
 		"app":                          "camunda-platform",
 		"app.kubernetes.io/name":       "camunda-platform",
 		"app.kubernetes.io/instance":   osc.Name,
+		"app.kubernetes.io/component":  "core",
 		"app.kubernetes.io/part-of":    "camunda-platform",
 		"app.kubernetes.io/managed-by": "orchestrationcluster-controller",
-		"app.kubernetes.io/component":  "core",
 	}
 }
